346af6d4ca9b4d669f83522bd3589a40a8c9b297
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-19 06:23:01 +0530

    Update addanime.py

--- plugins/addanime.py
+++ plugins/addanime.py
@@ -4,17 +4,17 @@ from pyrogram.types import Message
 
 
 @Bot.on_message(filters.command("addsub"))
-async def search_anime(client, message):
+async def addsub(client, message):
     await message.reply_text("hi")
 
 @Bot.on_message(filters.command("delsub"))
-async def search_anime(client, message):
+async def delsub(client, message):
     await message.reply_text("hi")
 
 @Bot.on_message(filters.command("adddub"))
-async def search_anime(client, message):
+async def adddub(client, message):
     await message.reply_text("hi")
 
 @Bot.on_message(filters.command("deldub"))
-async def search_anime(client, message):
+async def deldub(client, message):
     await message.reply_text("hi")
