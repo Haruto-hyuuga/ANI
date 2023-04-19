090c86dd8f0e1c5a6f1d0c626471d0820e32e2e5
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-19 06:41:21 +0530

    Update addanime.py

--- plugins/addanime.py
+++ plugins/addanime.py
@@ -2,16 +2,49 @@ from bot import Bot
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import ADMINS, PUBLIC_C_url, The_Other_Channel 
-from database.anime_db import present_sub_anime, get_sub_anime, present_dub_anime, get_dub_anime, add_sub_anime, del_sub_anime
+from database.anime_db import present_sub_anime, get_sub_anime, add_sub_anime, del_sub_anime
+from database.anime_db import present_dub_anime, get_dub_anime, add_bub_anime, del_bub_anime
 
 
 @Bot.on_message(filters.command("adddub"))
 async def adddub(client, message):
-    await message.reply_text("hi")
+    if message.reply_to_message:
+        link = message.reply_to_message.text
+        if len(message.command) != 1:
+            text = message.text.split(None, 1)[1]
+            anime_id = int(text) 
+            if not await present_dub_anime(anime_id):
+                try:
+                    await add_dub_anime(anime_id, link)
+                    await message.reply_text(f"<b>ADDED!</b>\n\nID: <b>{anime_id}</b>\nLINK: {link}")
+                except Exception as e:
+                    await message.reply_text(f"An Error Occured//-\n\n{e}")
+            else:
+                dblink = await get_dub_anime(anime_id)
+                await message.reply_text(f"<b>THIS ANIME ALREDY EXIST</b>\n\nID: <b>{anime_id}</b>\n<b>POST LINK:</b> {dblink}")
+        else:
+            await message.reply_text("<b>BISH PROVIDE ANIME ID AFTER COMMAND</b>\nTo Get Anime Id \nUse Command: /anime or /search")
+    else:
+        await message.reply_text(f"Bish Reply To Post Link From Channel:\n {The_Other_Channel}")
+        
 
 @Bot.on_message(filters.command("deldub"))
 async def deldub(client, message):
-    await message.reply_text("hi")
+    if len(message.command) != 1:
+        text = message.text.split(None, 1)[1]
+        anime_id = int(text) 
+        if await present_dub_anime(anime_id):
+            try:
+                dblink = await get_dub_anime(anime_id)
+                await del_dub_anime(anime_id)
+                await message.reply_text(f"<b>DELETED!</b>\n\nID: <b>{anime_id}</b>\n<b>POST LINK:</b> {dblink}")
+            except Exception as e:
+                await message.reply_text(f"An Error Occured//-\n\n{e}")
+        else:
+            await message.reply_text(f"No Such Anime Was Inserted In DataBase With ID: {anime_id}")
+    else:
+        await message.reply_text("<b>BISH PROVIDE ANIME ID AFTER COMMAND</b>\nTo Get Anime Id \nUse Command: /anime or /search")
+
 
 
 
