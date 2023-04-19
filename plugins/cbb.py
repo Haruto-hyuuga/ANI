29fa610e0fb0baf20be6459bdc57465f9fcf83a6
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-19 14:13:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -39,8 +39,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         await message.edit_text(
             text=f"{Caption}\n\n📬<b>REQUEST REGISTERED FOR THIS ANIME FOR SUB CHANNEL✅</n>"
         )
-        LOL = await Bot.send_photo(CREATOR_GC, photo=picc, caption=Caption)
-        await Bot.send_message(CREATOR_GC, text=f"👤{message.from_user.mention} \n<code>{message.from_user.id}</code>\n\n⚠️ REQUESTED ANIME FOR SUB CHANNEL", reply_to_message_id=LOL.id)
+        LOL = await Bot.send_photo(chat_id=CREATOR_GC, photo=picc, caption=Caption)
+        await Bot.send_message(chat_id=CREATOR_GC, text=f"👤{message.from_user.mention} \n<code>{message.from_user.id}</code>\n\n⚠️ REQUESTED ANIME FOR SUB CHANNEL", reply_to_message_id=LOL.id)
 
     elif data == "REQUEST_DA":
         message = query.message
@@ -49,8 +49,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         await message.edit_text(
             text=f"{Caption}\n\n📬<b>REQUEST REGISTERED FOR THIS ANIME FOR DUB CHANNEL✅</n>"
         )
-        LOL = await Bot.send_photo(CREATOR_GC, photo=picc, caption=Caption)
-        await Bot.send_message(CREATOR_GC, text=f"👤{message.from_user.mention} \n<code>{message.from_user.id}</code>\n\n⚠️ REQUESTED ANIME FOR DUB CHANNEL", reply_to_message_id=LOL.id)
+        LOL = await Bot.send_photo(chat_id=CREATOR_GC, photo=picc, caption=Caption)
+        await Bot.send_message(chat_id=CREATOR_GC, text=f"👤{message.from_user.mention} \n<code>{message.from_user.id}</code>\n\n⚠️ REQUESTED ANIME FOR DUB CHANNEL", reply_to_message_id=LOL.id)
 
     elif data == "anime_download_popup":
         await query.answer("To Download The Anime You Want Tap On The (/download 12345) And Send or You Can Use /find Command Followed By Anime Id From Anilist", show_alert=True)
