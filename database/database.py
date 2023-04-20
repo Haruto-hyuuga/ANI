2815dcbf39e158447e23de95d7e2a68b32cf7067
Rampage.heart <107468577+Lord-DSP-3@users.noreply.github.com>
2023-04-20 06:40:25 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -7,8 +7,6 @@ database = dbclient["CHAT"]
 
 
 user_data = database['Users']
-gc_data = database['Groups']
-
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
@@ -30,24 +28,3 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
 
-
-
-async def present_group(group_id : int):
-    found = gc_data.find_one({'_id': group_id})
-    return bool(found)
-
-async def add_group(group_id: int):
-    user_data.insert_one({'_id': group_id})
-    return
-
-async def full_group():
-    user_docs = gc_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
-    return user_ids
-
-async def del_group(group_id: int):
-    gc_data.delete_one({'_id': group_id})
-    return
