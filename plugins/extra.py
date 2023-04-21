a41c627dfb6750291426ac29dae849b5ef437032
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-21 17:25:26 +0530

    Update extra.py

--- plugins/extra.py
+++ plugins/extra.py
@@ -15,7 +15,12 @@ Title: {}
 """
 
 
-@Bot.on_message(filters.group & filters.new_chat_members)
+def is_group(_, __, message: Message):
+    return message.chat.type in ['group', 'supergroup']
+
+
+
+@Bot.on_message(is_group & filters.new_chat_members)
 async def leave_group(client, message: Message):
     
     added_by = [user.id for user in message.new_chat_members if user.is_bot]
@@ -24,7 +29,14 @@ async def leave_group(client, message: Message):
     TGC_TT = message.chat.title
     FINAL_GIF = await Gif_Random()
 
+    if ADMINS in added_by:
+
+        THANKS_MSG = "<b>Thanks For Having Me In:</b> {}"
+        await client.send_animation(chat_id=TGC_id, animation=FINAL_GIF, caption=THANKS_MSG.format(TGC_TT))
+        pass
+
     if ADMINS not in added_by:
+
         ALLCC_MSG = "<b>FEEL FREE TO USE ME IN PRIVATE CHAT</b>\n𝙃𝙚𝙧𝙚'𝙨 𝙏𝙝𝙚 𝙇𝙞𝙨𝙩 𝙊𝙛 𝘼𝙡𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡𝙨:"
         try:
             await client.send_animation(chat_id=TGC_id, animation=FINAL_GIF, caption=ALLCC_MSG, reply_markup=AllFSCB)    
@@ -33,11 +45,7 @@ async def leave_group(client, message: Message):
         except Exception as e:
             await cleint.send_message(chat_id=REQUEST_GC, text=f"⚠️AUTO LEAVE UNAUTH GROUP\nwhile Leaving Group\n\n{e}", reply_to_message_id=ERR_TOPIC_ID)
 
-    else:
-        THANKS_MSG = "<b>Thanks For Having Me In:</b> {}"
-        await client.send_animation(chat_id=TGC_id, animation=FINAL_GIF, caption=THANKS_MSG.format(TGC_TT))
-        pass
-
+        
 
 
 
