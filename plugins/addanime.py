085bf941a721c10be86f7237c129591bcf8593b3
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-22 13:02:00 +0530

    Update addanime.py

--- plugins/addanime.py
+++ plugins/addanime.py
@@ -315,8 +315,8 @@ async def fchannelSUBpost(client, message):
     MAX_GENRES_LEN = 30 
     genres = ", ".join(anime["genres"])
     if len(genres) > MAX_GENRES_LEN:
-    split_genres = [f"<i>{genre}</i>" if genre.strip() in anime["genres"] else genre for genre in genres.split(",")]
-    genres = "\n│ ".join([", ".join(split_genres[:MAX_GENRES_LEN]), ", ".join(split_genres[MAX_GENRES_LEN:])])
+        split_genres = [f"<i>{genre}</i>" if genre.strip() in anime["genres"] else genre for genre in genres.split(",")]
+        genres = "\n│ ".join([", ".join(split_genres[:MAX_GENRES_LEN]), ", ".join(split_genres[MAX_GENRES_LEN:])])
 
     if "studios" in anime and anime["studios"] and "edges" in anime["studios"] and anime["studios"]["edges"] and len(anime["studios"]["edges"]) > 0 and "node" in anime["studios"]["edges"][0] and anime["studios"]["edges"][0]["node"] and "name" in anime["studios"]["edges"][0]["node"]:
         studio = anime["studios"]["edges"][0]["node"]["name"]
@@ -428,8 +428,8 @@ async def fchannelDuBpost(client, message):
     MAX_GENRES_LEN = 30 
     genres = ", ".join(anime["genres"])
     if len(genres) > MAX_GENRES_LEN:
-    split_genres = [f"<i>{genre}</i>" if genre.strip() in anime["genres"] else genre for genre in genres.split(",")]
-    genres = "\n│ ".join([", ".join(split_genres[:MAX_GENRES_LEN]), ", ".join(split_genres[MAX_GENRES_LEN:])])
+        split_genres = [f"<i>{genre}</i>" if genre.strip() in anime["genres"] else genre for genre in genres.split(",")]
+        genres = "\n│ ".join([", ".join(split_genres[:MAX_GENRES_LEN]), ", ".join(split_genres[MAX_GENRES_LEN:])])
 
     if "studios" in anime and anime["studios"] and "edges" in anime["studios"] and anime["studios"]["edges"] and len(anime["studios"]["edges"]) > 0 and "node" in anime["studios"]["edges"][0] and anime["studios"]["edges"][0]["node"] and "name" in anime["studios"]["edges"][0]["node"]:
         studio = anime["studios"]["edges"][0]["node"]["name"]
