6ab657d8febd74864cc888e42d8e2c61cce55ffe
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-22 21:13:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -87,7 +87,7 @@ async def cb_handler(client, query: CallbackQuery):
         await M.edit_text("<b>POSTED SUCCESSFULLY ON SUB CHANNEL ✅</b>\n🔗: @ANIME_DOWNLOADS_SUB")
         await query.message.edit_text(f"<i>REPLY TO POST LINK BY COMMAND:</i>\n\n👉🏻  <code>/addsub {anime_id}</code>")    
 
-        await client.send_message(DUB_CHANNEL, text="➖➖➖➖🖥️➖➖➖➖")
+        await client.send_message(SUB_CHANNEL, text="➖➖➖➖🖥️➖➖➖➖")
         await asyncio.sleep(15)
         await query.message.reply_text(f"{Sub_C_url}/{P.id}", disable_web_page_preview=True)
         await client.send_message(query.message.chat.id, text="➖➖➖➖👆🏻➖➖➖➖")
