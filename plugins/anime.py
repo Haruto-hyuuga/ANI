0284647a7b26bd77aa14eb2dc02dc28e9833e75b
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-24 07:49:38 +0530

    Update anime.py

--- plugins/anime.py
+++ plugins/anime.py
@@ -5,8 +5,7 @@ from database.inline import ERROR_BUTTON, ANIME_RESULT_B
 from config import FS_GROUP, ALLCMD_FS_TXT, ALLCMD_FS_PIC, ERR_TOPIC_ID, REQUEST_GC
 
 from helper_func import sub_PUB_Sc, sub_PUB_Dc, sub_BOT_c, sub_GC
-from req import get_full_anime_info, channel_post_anime_info, search_find_anime_list, search_anime_list_by_Name
-
+from req import get_full_anime_info, channel_post_anime_info, search_find_anime_list, search_anime_list_by_Name, full_info_anime_list_by_Name, download_anime_buttons_db
 
 
 
