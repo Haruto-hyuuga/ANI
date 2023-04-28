2af9855832a0a42719799268340fd9b993c4bc89
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-28 14:15:27 +0530

    Update groupstuff.py

--- plugins/groupstuff.py
+++ plugins/groupstuff.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters, errors
 from pyrogram.types import Message
 
 import asyncio
-from config import ADMINS, BOT_ID
+from config import ADMINS, BOT_ID, REQUEST_GC, ERR_TOPIC_ID
 from config import USER_LOG_CHANNEL as LOGG
 from database.database import present_chat, add_chat, full_chatbase, del_chat
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, ChatWriteForbidden, BotKicked, UserNotParticipant
@@ -20,56 +20,46 @@ __CHAT ID:__ `{}`
 GROUP_TEXT = ""
 
 
-async def get_invite_link(client, id):
-
-
-
-
-@Bot.on_message(filters.command(["start", "help"]) & filters.group)
-async def on_start(client, message):
-    id = message.chat.id
-    name = message.chat.title
-    uname = message.chat.username
-    h_id = f"#ID{(-1*id)}"
-    try:
-        link = await client.export_chat_invite_link(id)
-    except errors.ChatAdminRequired as e:
-        link = "https://t.me/c/1909557377/9"
-    except errors:
-        link = "https://t.me/c/1909557377/22"
-
-    LOG_BT = await Ani_log_group(link)
-    if not await present_chat(id):
-        try:
-            await add_chat(id)
-            await client.send_message(LOGG, text=NCL_txt.format(name, uname, id, h_id), reply_markup=LOG_BT)
-        except:
-            pass
-    video = await Vid_Random()
-    await client.send_video(id, video, caption=GROUP_TEXT, reply_markup=GC_START_B)
-    
- 
-@Bot.on_message(filters.new_chat_members & filters.user(BOT_ID))
-async def welcome(client, message):
-    chat_id = message.chat.id
-    name = message.chat.title
-    uname = message.chat.username
-    h_id = f"#ID{(-1*chat_id)}"
+async def new_gc_logger(client, chat_id, N, UN):
     try:
         link = await client.export_chat_invite_link(chat_id)
     except errors.ChatAdminRequired as e:
         link = "https://t.me/c/1909557377/9"
     except errors:
         link = "https://t.me/c/1909557377/22"
-
     LOG_BT = await Ani_log_group(link)
- 
+    h_id = f"#ID{(-1*chat_id)}"
     if not await present_chat(chat_id):
-        try:
-            await add_chat(chat_id)
-            await client.send_message(LOG_CHANNEL, text=NCL_txt.format(name, uname, chat_id, h_id), reply_markup=LOG_BT)
-        except:
-            pass
+        await add_chat(chat_id)
+        await client.send_message(LOGG, text=NCL_txt.format(N, UN, chat_id, h_id), reply_markup=LOG_BT)
+
+            
+
+
+@Bot.on_message(filters.command(["start", "help"]) & filters.group)
+async def on_startgclog(client, message):
+    id = message.chat.id
+    N = message.chat.title
+    UN = message.chat.username
+    try:
+        await new_gc_logger(client, id, N, UN)
+    except Exception as e:
+        await client.send_message(REQUEST_GC, text=f"⚠️NEW GC LOG\n\n{e}", reply_to_message_id=ERR_TOPIC_ID)
+
+    video = await Vid_Random()
+    await client.send_video(id, video, caption=GROUP_TEXT, reply_markup=GC_START_B)
+    
+ 
+@Bot.on_message(filters.new_chat_members & filters.user(BOT_ID))
+async def welcomenewgc(client, message):
+    id = message.chat.id
+    N = message.chat.title
+    UN = message.chat.username
+
+    try:
+        await new_gc_logger(client, id, N, UN)
+    except Exception as e:
+        await client.send_message(REQUEST_GC, text=f"⚠️NEW GC LOG\n\n{e}", reply_to_message_id=ERR_TOPIC_ID)
 
     for member in message.new_chat_members:
         if member.id == BOT_ID:
