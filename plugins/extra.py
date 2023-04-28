f5f2ec10a6146ddbc4afece43a7de32c8725d069
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-28 14:48:12 +0530

    Update extra.py

--- plugins/extra.py
+++ plugins/extra.py
@@ -5,7 +5,7 @@ from config import ADMINS, Gif_Random, REQUEST_GC, ERR_TOPIC_ID, USER_LOG_CHANNE
 from database.inline import AllFSCB
 from database.user_stats import get_user_Ani_Id, update_Anid
 from database.req_Db import full_requestDB_DUB, full_requestDB_SUB, del_DUB_request, del_SUB_request
-from req import search_user_name, search_user_id
+from req import search_user_name, search_user_id, get_cmd
 
 GC_LOG_TXT = """
 🔴 #New_GROUP
@@ -16,36 +16,8 @@ Title: {}
 🚷 LEFT GROUP ✅
 """
 
-"""
-
-@Bot.on_message(filters.group & filters.new_chat_members)
-async def leave_group(client, message: Message):
-    
-    added_by = [user.id for user in message.new_chat_members if user.is_bot]
-    TGC_id = message.chat.id
-    TGC_Lk = message.chat.username
-    TGC_TT = message.chat.title
-    FINAL_GIF = await Gif_Random()
-
-    if ADMINS in added_by:
-
-        THANKS_MSG = "<b>Thanks For Having Me In:</b> {}"
-        await client.send_animation(chat_id=TGC_id, animation=FINAL_GIF, caption=THANKS_MSG.format(TGC_TT))
-        pass
-
-    if ADMINS not in added_by:
-
-        ALLCC_MSG = "<b>FEEL FREE TO USE ME IN PRIVATE CHAT</b>\n𝙃𝙚𝙧𝙚'𝙨 𝙏𝙝𝙚 𝙇𝙞𝙨𝙩 𝙊𝙛 𝘼𝙡𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡𝙨:"
-        try:
-            await client.send_animation(chat_id=TGC_id, animation=FINAL_GIF, caption=ALLCC_MSG, reply_markup=AllFSCB)    
-            await client.send_message(chat_id=USER_LOG_CHANNEL, text=GC_LOG_TXT.format(TGC_TT, TGC_id, TGC_id, TGC_Lk))
-            await client.leave_chat(TGC_id)
-        except Exception as e:
-            await cleint.send_message(chat_id=REQUEST_GC, text=f"⚠️AUTO LEAVE UNAUTH GROUP\nwhile Leaving Group\n\n{e}", reply_to_message_id=ERR_TOPIC_ID)
-
-"""
         
-@Bot.on_message(filters.command("auth") & filters.private)
+@Bot.on_message(get_cmd("auth") & filters.private)
 async def auth_ani_acc(client, message):
     UID = message.from_user.id
     args = message.text.split()
@@ -93,7 +65,7 @@ async def auth_ani_acc(client, message):
             await cleint.send_message(chat_id=REQUEST_GC, text=f"⚠️while Adding User Anilist\n\n{e}", reply_to_message_id=ERR_TOPIC_ID)
 
 
-@Bot.on_message(filters.command("unauth") & filters.private)
+@Bot.on_message(get_cmd("unauth") & filters.private)
 async def delete_anilist_acc(client, message):
     UID = message.from_user.id
     try:
@@ -109,7 +81,7 @@ async def delete_anilist_acc(client, message):
         await cleint.send_message(chat_id=REQUEST_GC, text=f"⚠️while Adding User Anilist\n\n{e}", reply_to_message_id=ERR_TOPIC_ID)
 
 
-@Bot.on_message(filters.command("reqlist") & filters.user(ADMINS))
+@Bot.on_message(get_cmd("reqlist") & filters.user(ADMINS))
 async def pending_req_list(client, message):
 
     Squery = await full_requestDB_SUB()
@@ -129,7 +101,7 @@ async def pending_req_list(client, message):
 
 
 
-@Bot.on_message(filters.command("delsreq") & filters.user(ADMINS))
+@Bot.on_message(get_cmd("delsreq") & filters.user(ADMINS))
 async def delete_subreq_list(client, message):
     ani = message.text.split(None, 1)[1]
     try:
@@ -139,7 +111,7 @@ async def delete_subreq_list(client, message):
     except Exception as e:
         await message.reply_text(e)
 
-@Bot.on_message(filters.command("deldreq") & filters.user(ADMINS))
+@Bot.on_message(get_cmd("deldreq") & filters.user(ADMINS))
 async def delete_dubreq_list(client, message):
     ani = message.text.split(None, 1)[1]
     try:
