0fdd28c5083334b634f327525941d25a620d88fb
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-07-06 09:40:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import logging
 from logging.handlers import RotatingFileHandler
 import random 
 
-#BOT 1
-BOT_ID = 6116589740
-BOTUSERNAME = "AnimeDL_Robot"
-TG_BOT_TOKEN = "6116589740:AAEZ4AARouWW0nFsN7YfffeqOIU3sCwrxW4"
-USER_LOG_CHANNEL = -1001909557377
-DB_URI = "mongodb+srv://vagil76793:cO7oFNJzbCksNKkK@cluster0.cvj8wvf.mongodb.net/?retryWrites=true&w=majority"
+#BOT = 1
+#BOT_ID = 6116589740
+#BOTUSERNAME = "AnimeDL_Robot"
+#TG_BOT_TOKEN = "6116589740:AAEZ4AARouWW0nFsN7YfffeqOIU3sCwrxW4"
+#USER_LOG_CHANNEL = -1001909557377
+#DB_URI = "mongodb+srv://vagil76793:cO7oFNJzbCksNKkK@cluster0.cvj8wvf.mongodb.net/?retryWrites=true&w=majority"
 
-#BOT 2
-#BOT_ID = 6286463449
-#BOTUSERNAME = "AnimeDownLoader_Robot"
-#TG_BOT_TOKEN = "6286463449:AAGThxmuOUis9HN_IYMCvZdcwjKeOOw3U7o"
-#USER_LOG_CHANNEL = -1001925844286
-#DB_URI = "mongodb+srv://tivato1695:1uhFj9UbQMgHGm3O@cluster0.o0kxhwr.mongodb.net/?retryWrites=true&w=majority"
+#BOT = 2
+BOT_ID = 6286463449
+BOTUSERNAME = "AnimeDownLoader_Robot"
+TG_BOT_TOKEN = "6286463449:AAGThxmuOUis9HN_IYMCvZdcwjKeOOw3U7o"
+USER_LOG_CHANNEL = -1001925844286
+DB_URI = "mongodb+srv://tivato1695:1uhFj9UbQMgHGm3O@cluster0.o0kxhwr.mongodb.net/?retryWrites=true&w=majority"
 
 
 APP_ID = 12585681
